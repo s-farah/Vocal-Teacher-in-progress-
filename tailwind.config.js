cbb757fc299037222205f591f5faaf865d003961
Mireya <mireyazamora@berkeley.edu>
2025-10-25 21:41:13 -0700

    trying to fix mistakes
    trying to fix an error I made

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-export default {
-    content: ["./index.html", "./src/**/*.{js,jsx}"],
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
     theme: { extend: {}, },
     pulgins: [],
-}
+};
