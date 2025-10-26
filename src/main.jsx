3092d7ea354b9fb9e4bda78461602b8f396d7d1f
Mireya <mireyazamora@berkeley.edu>
2025-10-25 23:57:38 -0700

    checking for issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <ReactDOM.StrictMode>
-    <App />
-  </ReactDOM.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <ReactDOM.StrictMode>
+//     <App />
+//   </ReactDOM.StrictMode>,
+// )
